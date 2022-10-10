9db5b350c3706960496802d15e720d5f3edcdecf
sbawane1212 <115446841+sbawane1212@users.noreply.github.com>
2022-10-10 22:45:02 +0930

    Update share.groovy

--- Vars/share.groovy
+++ Vars/share.groovy
@@ -2,15 +2,3 @@ def sum(a,b)
 {
   echo "the sum of ${a} and ${b} is ${a+b}"
 }
-def substraction(a,b)
-{
-  echo "the substraction of ${a} and ${b} is ${a-b}"
-}
-def divide(a,b)
-{
-  echo "the divide of ${a} and ${b} is ${a/b}"
-}
-def multiplication(a,b)
-{
-  echo "the multiplication of ${a} and ${b} is ${a*b}"
-}
